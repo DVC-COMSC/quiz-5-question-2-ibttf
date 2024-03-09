8a8ca15af00e3038879c9d9f8cd3b37a84d708c0
roylee0912 <leeroy0912@gmail.com>
2024-03-08 17:07:19 -0800

    done

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ int main()
    
    cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
    cin >> N >> M;
+   for (int i = 0; i <= N; i++){
+      int temp=1;
+      for (int j=1; j<=i; j++){
+         temp = temp*N;
+      }
+      cout << temp << endl;
+
+   }
    /* Complete your code */
   
 }
