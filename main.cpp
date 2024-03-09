3062416963e54a0c4e78f3fe573c9ab76377e22c
roylee0912 <leeroy0912@gmail.com>
2024-03-08 17:08:29 -0800

    think i fixed?

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
    
    cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
    cin >> N >> M;
-   for (int i = 0; i <= N; i++){
+   for (int i = 0; i <= M; i++){
       int temp=1;
       for (int j=1; j<=i; j++){
          temp = temp*N;
